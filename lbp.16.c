6a00c2fe7971306525b9bbf6f4a74d0a675a6395
valehasini <valehasini@gmail.com>
2025-03-24 20:21:44 +0530

    Add files via upload

---
+++ lbp.16.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+ float kilometers;
+ printf("input distance in kilometers:");
+ scanf("%f",&kilometers);
+ printf("value in miles is %f",kilometers*0.62);
+ printf(".");
+ return 0;
+}
+
---
+++ lbp.17.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ float b,v;
+ int h;
+ printf("enter the value of b:");
+ scanf("%f",&b);
+ printf("enter the value of h:");
+ scanf("%f",&h);
+ v=b*h/3;	
+ printf("%f",v);
+ return 0;
+}
